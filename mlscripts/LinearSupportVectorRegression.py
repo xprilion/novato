b1047b7208bafc952794f2f211702e3324a099a3
Unknown <sharnagat.mech97@gmail.com>
2017-08-31 20:31:54 +0530

    models

---
+++ mlscripts/LinearSupportVectorRegression.py
@@ -0,0 +1,27 @@
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.svm import SVR
+ 
+np.random.seed(0) 
+X = np.sort(5 * np.random.rand(40, 1), axis=0) 
+T = np.linspace(0, 5, 500)[:, np.newaxis] 
+y = np.sin(X).ravel() 
+ 
+# Add noise to targets 
+y[::5] += 1 * (0.5 - np.random.rand(8)) 
+ 
+# Fit RBF Model 
+linear_svm = int(input('n_neighbour parameters:')) 
+for i, weights in enumerate(['uniform', 'distance']): 
+    svr_lin = SVR(kernel='linear',C=1e3)
+    y_ = svr_lin.fit(X,y).predict(T)
+     
+plt.subplot(2, 1, i + 1) 
+plt.scatter(X, y, c='k', label='data') 
+plt.plot(T, y_, c='b', label='prediction') 
+plt.axis('tight') 
+plt.legend() 
+plt.title("LinearSupportVectorRegression (k = %i, weights = '%s')" % (linear_svm, 
+weights)) 
+plt.show() 
